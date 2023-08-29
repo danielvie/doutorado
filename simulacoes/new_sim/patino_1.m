cfbdfdf97c55ec7494bb40da95207f5d8c058a06
Daniel Vieira <danielvie@gmail.com>
2023-08-30 01:52:16 +0200

    pre-reuniao 31 ago

--- simulacoes/new_sim/patino_1.m
+++ simulacoes/new_sim/patino_1.m
@@ -17,13 +17,24 @@ c.x0 = c.x0;
 nsim = 30;
 [y,t,u,~] = engine.sim_n(c, nsim);
 
-% plot dos resultados
+%% plot dos resultados
+figure(1);
 hold off;
 f = plot(y(:,1), y(:,2), 'LineWidth', 2);
 hold on;
 plot(c.xref(1), c.xref(2), 'rx');
 axis equal;
 grid on;
-xlabel('x1 - Capacitor voltage');
-ylabel('x2 - Inductor current');
+xlabel('x1 - xapacitor voltage');
+ylabel('x2 - inductor current');
+set(gca,'fontsize', 15);
+
+figure(2);
+hold off;
+
+i = t < c.Ts(end);
+f = plot(t(i), u(i), 'linew', 2);
+grid on;
+xlabel('t - time(s)');
+ylabel('u - input command');
 set(gca,'fontsize', 15);
