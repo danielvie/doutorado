6f93914206fb23ef70ad80037eb87988b91a2982
Daniel Vieira <danielvie@gmail.com>
2023-11-29 00:50:39 +0100

    pre-reuniao 29 nov

---
+++ projecao/test.m
@@ -0,0 +1,120 @@
+                                                                                            
+% aula 8
+
+% modelo discretizado com T = 0.1s
+A = [1, 0.1; 0, 1];
+B = [5*1e-3;0.1];
+C = [1, 0];
+
+% ganho de realimentacao K usando DLQR
+Q = [50, 0; 0, 1];
+R = 1;
+Pf = diag([1, 1]);
+
+% restricoes
+Sx = [1, 0; -1, 0];
+bx = [4*pi/6; pi/6];
+Su = [1;-1];
+bu = [5; 2];
+
+N = 10;
+
+
+% a projecao pode ser realizada empregando a funcao 'projection' do MPT Toolbox
+% {aula8, slide 33}
+
+
+% sendo Sf, bf obtidos na caracterizacao de O_inf com os valores de equilibrio xbar, ubar considerados
+% {aula8, slide 10}
+
+% aux...
+Qn = Q; Sxn = Sx;
+for i = 2:N-1
+    Qn = blkdiag(Qn, Q);
+    Sxn = blkdiag(Sxn, Sx);
+end
+Qn = blkdiag(Qn, Pf);
+Sxn = blkdiag(Sxn, Sf);
+bxn = [repmat(bx,N-1,1); bf];
+
+An = A; Rn = R; Sun = Su;
+for i = 2:N
+   An = [An;A^i];
+   Rn = blkdiag(Rn,R);
+   Sun = blkdiag(Sun,Su);
+end
+bun = repmat(bu,N,1);
+
+
+n = size(B, 1);
+p = size(B, 2);
+Baux = mat2cell(zeros(n*N, p*N), n*ones(N,1), p*ones(1,N));
+for i = 1:N
+    for j = 1:i
+        Baux{i,j} = (A^(i-j))*B;
+    end
+end
+Bn = cell2mat(Baux);
+
+
+% projection
+Sw = [Sxn*Bn Sxn*An;
+      Sun zeros(size(Sun, 1), n);
+      zeros(size(Sx,1), p*N) Sx];
+      
+bw = [bxn; bun; bx];
+
+Pw = Polyhedron('H', [Sw bw]);
+
+D = projection(Pw, p*N+1:p*N+n);
+
+
+
+
+
+
+
+function un = dlqrcon(A,B,Q,R,Pf,N,Sx,bx,Su,bu,Sf,bf,x0,xbar,ubar)
+
+    An = A; Rn = R; Sun = Su;
+    
+    for i = 2:N
+        An = [An; A^i];
+        Rn = blkdiag(Rn, R);
+        Sun = blkdiag(Sun, Su);
+    end
+    bun = repmat(bu, N, 1);
+
+    n = size(B, 1);
+    p = size(B, 2);
+
+    Baux = mat2cell(zeros(n*N, p*N), n*ones(N, 1), p*ones(1, N));
+    for i = 1:N
+        for j = 1:i
+            Baux{i,j} = (A^(i-j))*B;
+        end
+    end
+    Bn = cell2mat(Baux);
+
+    Qn = Q; Sxn = Sx;
+    for i = 2:N-1
+        Qn = blkdiag(Qn, Q);
+        Sxn = blkdiag(Sxn, Sx);
+    end
+    Qn = blkdiag(Qn, P);
+    Sxn = blkdiag(Sxn, Sf);
+    bxn = [repmat(bx,N-1,1);bf];
+
+    % uso do quadprog
+    Aqp = [Sxn*Bn; Sun];
+    bqp = [bxn - Sxn*An*x0;bun];
+
+    Hqp = Bn'*Qn*Bn + Rn;
+    
+    % Simetrizacao
+    Hqp = (Hqp + Hqp')/2;
+    
+    fqp = (Bn'*Qn*(An*x0 - repmat(xbar,N,1)) - Rn*repmat(ubar, N, 1));
+    
+    un = quadprog(Hqp, fqp, Aqp, bqp);
+end
---
+++ z_tutoriais/play/invert_matrix.m
@@ -0,0 +1,3 @@
+function [Mi] = invert_matrix(M)
+    Mi = inv(M);
+end
