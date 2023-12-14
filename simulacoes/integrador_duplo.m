5763d0fc3e4828ac5cdf220aff4ff283b4c4706a
Daniel Vieira <daniel.vieira@thermofisher.com>
2023-12-14 19:09:56 +0100

    fix figura de modos

--- simulacoes/integrador_duplo.m
+++ simulacoes/integrador_duplo.m
@@ -126,15 +126,25 @@ function plot_traj(y, x0, tit, x_label, y_label)
 end
 
 function plot_control_signal(t_off, m_off, t, m, tit, x_label, y_label)
-    stairs(t_off, m_off, 'linew', 2);
+    f = stairs(t_off, m_off, 'linew', 2);
     hold on;
     stairs(t, m, 'linew', 2, 'linestyle', '--');
     hold off;
-    % xlim([0, 25]);
+    
     grid on;
     title(tit);
     xlabel(x_label);
     ylabel(y_label);
     set(gca,'fontsize', 15);
     legend('modes target trajectory', 'modes simulation');
+
+    % removing the values that are not integers in Y axis
+    v = f.Parent.YTick;
+    
+    % indices for integer
+    p = abs(v - floor(v)) < 0.1;
+    
+    % update yticks
+    f.Parent.YTick = v(p);
+
 end
