806da09ccff3f168cd103da4cb732db3c52ea35b
Daniel Vieira <danielvie@gmail.com>
2023-12-20 19:00:40 +0100

    pre-reuniao 20 dez

--- simulacoes/patino_2_mpc.m
+++ simulacoes/patino_2_mpc.m
@@ -107,62 +107,29 @@ function var_out = patino_2_mpc(save_fig)
         
     %% 5 PLOTING GRAPHICS
 
-        % renomeando variaveis
-        vc1 = y(:,1);
-        vc2 = y(:,2);
-        i_l = y(:,3);
-
         % plot dos resultados
-        close all;
+        % close all;
 
+        % state x1
         f1 = figure(1);
-        plot(t, vc1);
+        plot_y(t, y(:,1), 'Voltage C_1 Multilevel Converter', 't - time(s)', 'V_{c1} [V]')
 
-        grid on;
-        xlabel('t - time(s)');
-        ylabel('V_{c1} [V]');
-        set(gca,'fontsize', 15);
-
-
+        % state x2
         f2 = figure(2);
-        plot(t, vc2);
+        plot_y(t, y(:,2), 'Voltage C_2 Multilevel Converter', 't - time(s)', 'V_{c2} [V]')
 
-        grid on;
-        xlabel('t - time(s)');
-        ylabel('V_{c2} [V]');
-        set(gca,'fontsize', 15);
-
+        % state x3
         f3 = figure(3);
-        plot(t, i_l);
-
-        grid on;
-        xlabel('t - time(s)');
-        ylabel('i_L [A]');
-        set(gca,'fontsize', 15);
+        plot_y(t, y(:,3), 'Current i_L Multilevel Converter', 't - time(s)', 'i_{L} [A]')
 
         f4 = figure(4);
-        plot3(vc1, vc2, i_l, 'k');
+        plot_traj(y, y_off, config.x0, "Multilevel Converter Trajectory", "V_{c1} [V]", "V_{c2} [V]", "I_{L} [A]")
 
-        grid on;
-        xlabel('V_{c1} [V]');
-        ylabel('V_{c2} [V]');
-        zlabel('i_L [A]');
-        set(gca,'fontsize', 15);
-
-        % plotando diferencas
-
+        % control signal
         f5 = figure(5);
-        stairs(t_off, m_off, 'linew', 2);
-        hold on;
-        stairs(t, m, 'linew', 2, 'linestyle', '--');
-        hold off;
-        xlim([0, 1.46e-3]);
-        grid on;
-        xlabel('time (s)');
-        ylabel('mode');
-        set(gca,'fontsize', 15);
-        legend('modes target trajectory', 'modes simulation');
-
+        ncycle = size(y_off, 1) / nsim;
+        ncycle = ncycle * 6 -700;
+        plot_control_signal(t_off(1:ncycle), m_off(1:ncycle), t(1:ncycle), m(1:ncycle), "Multilevel Converter Control Signal", "time (s)", "mode")
 
         % get values from the function
         all_variables = who;
@@ -182,6 +149,85 @@ function var_out = patino_2_mpc(save_fig)
             save_figure(f4, "graf_patino_ex2_4.pdf", addr_out);
             save_figure(f5, "graf_patino_ex2_5.pdf", addr_out);
         end
+end
 
+function plot_y(t, yi, tit, x_label, y_label)
+    plot(t, yi);
+
+    grid on;
+    title(tit)
+    xlabel(x_label);
+    ylabel(y_label);
+    set(gca,'fontsize', 15);
+
+end
+
+function plot_traj(y, y_off, x0, tit, x_label, y_label, z_label)
+
+    yo1 = y_off(:,1);
+    yo2 = y_off(:,2);
+    yo3 = y_off(:,3);
+
+    y1 = y(:,1);
+    y2 = y(:,2);
+    y3 = y(:,3);
+
+    plot3(yo1, yo2, yo3);
+    hold on;
+    plot3(y1, y2, y3);
+
+    % plot marcadores
+    plot3(y1(1), y2(1), y3(1), 'ro', 'markers', 15, 'linew', 2); % start point
+    plot3(yo1(end), yo2(end), yo3(end), 'k*', 'markers', 15, 'linew', 2); % end point
+    plot3(y1(end), y2(end), y3(end), 'k.', 'markers', 50); % end point
+    plot3(x0(1), x0(2), x0(3), 'rx', 'markers', 25, 'linew', 3); % target x_0
+
+    legend('trajectory MPC off', 'trajectory MPC on', 'start', 'end MPC off', 'end MPC on', 'target x_0', 'location', 'southeast');
+
+    hold off;
+    grid on;
+    title(tit)
+    xlabel(x_label);
+    ylabel(y_label);
+    zlabel(z_label);
+    set(gca,'fontsize', 15);
+
+end
+
+function plot_control_signal(t_off, m_off, t, m, tit, x_label, y_label)
+
+
+        % stairs(t_off, m_off, 'linew', 2);
+        % hold on;
+        % stairs(t, m, 'linew', 2, 'linestyle', '--');
+        % hold off;
+        % xlim([0, 1.46e-3]);
+        % grid on;
+        % xlabel('time (s)');
+        % ylabel('mode');
+        % set(gca,'fontsize', 15);
+        % legend('modes target trajectory', 'modes simulation');
+
+
+    f = stairs(t_off, m_off, 'linew', 2);
+    hold on;
+    stairs(t, m, 'linew', 2, 'linestyle', '--');
+    hold off;
+    
+    grid on;
+    title(tit);
+    xlabel(x_label);
+    ylabel(y_label);
+    set(gca,'fontsize', 15);
+    legend('modes target trajectory', 'modes simulation');
+
+    % removing the values that are not integers in Y axis
+    v = f.Parent.YTick;
+    
+    % indices for integer
+    p = abs(v - floor(v)) < 0.1;
+    
+    % update yticks
+    f.Parent.YTick = v(p);
 
 end
