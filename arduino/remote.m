ebd0b25dc85d68b4a83ddca99bc8e7842f4371b1
Daniel Vieira <danielvie@gmail.com>
2024-07-11 08:59:58 -0300

    iter

--- arduino/remote.m
+++ arduino/remote.m
@@ -11,18 +11,18 @@ arduinoSerial = serialport(comPort, baudRate);
 % Open the serial connection
 fopen(arduinoSerial);
 
+
+% send two vectors to the arduino
+timeVector = [100, 200, 300, 400];
+intVector = [1, 2, 3, 4];
+sendVectors(arduinoSerial, timeVector, intVector);
+
+
 % Example usage: Change the delay interval to 500 ms
-changeDelayInterval(arduinoSerial, 5000);
+% changeDelayInterval(arduinoSerial, 5000);
 
 % Continuously read and display the analog value from the Arduino
-disp('Reading analog values from Arduino:');
-while true
-    if arduinoSerial.NumBytesAvailable > 0
-        analogValue = readline(arduinoSerial);
-        fprintf('Analog Value: %s\n', analogValue);
-    end
-    pause(0.1); % Small delay to prevent overwhelming the serial port
-end
+listenSerial(arduinoSerial, 0.1);
 
 % Close the serial connection
 clear arduinoSerial;
@@ -31,4 +31,28 @@ clear arduinoSerial;
 function changeDelayInterval(serialObj, interval)
     % Convert the interval to a string and send it over serial
     writeline(serialObj, num2str(interval));
+end
+
+function listenSerial(arduinoSerial, period_sec)
+    disp('Reading analog values from Arduino:');
+    while true
+        if arduinoSerial.NumBytesAvailable > 0
+            analogValue = readline(arduinoSerial);
+            fprintf('Analog Value: %s\n', analogValue);
+        end
+        pause(period_sec); % Small delay to prevent overwhelming the serial port
+    end
+end
+
+function sendVectors(serialObj, timeVector, intVector)
+    % convert the vectors to comma-separated strings
+    % 100,200,300,400;1,2,3,4
+    timeStr = strjoin(string(timeVector), ",");
+    intStr = strjoin(string(intVector), ",");
+
+    % concatenate the strings with a semicolon separator
+    dataStr = strcat(timeStr, ";", intStr);
+
+    % send string
+    writeline(serialObj, dataStr);
 end
