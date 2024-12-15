612484261f0cb8380de76bedb33b9e4fc2aa1ced
Daniel Vieira <danielvie@gmail.com>
2024-12-15 00:45:15 -0300

    add parseSignal, that parses vector

--- arduino/cpp/get_values/src/main.cpp
+++ arduino/cpp/get_values/src/main.cpp
@@ -1,24 +1,78 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
 
 void PrintV(int V[], int n);
 void GetValues(const char* s, int timeValues[], int modeValues[]);
 
+void parseSignal(const std::string &s, std::vector<int64_t> &time, std::vector<int64_t> &mode);
+void _parseSection(const std::string &section, std::vector<int64_t> &result);
+
 int main(int argc, char const *argv[])
 {
-    std::string s = "100,200,300,400;1,2,3,4";
+    std::string s = "100,2,548,452,546540,5462,4564516,4562,00,300,400;1,4,1,5,1,85,3,1325,458,12,2,3,4";
     
-    int timeValues[4];
-    int modeValues[4];
+    // int timeValues[4];
+    // int modeValues[4];
     
-    GetValues(s.c_str(), timeValues, modeValues);
-    PrintV(timeValues, 4);
-    PrintV(modeValues, 4);
+    // GetValues(s.c_str(), timeValues, modeValues);
+    // PrintV(timeValues, 4);
+    // PrintV(modeValues, 4);
+    
+    std::vector<int64_t> time;
+    std::vector<int64_t> mode;
+    
+    parseSignal(s, time, mode);
 
     std::cout << s << std::endl;
+    
+    // printing time
+    std::cout << "time: ";
+    for (auto ti : time) {
+        std::cout << ti << ", ";
+    }
+    std::cout << "\n";
+
+    // printing mode
+    std::cout << "mode: ";
+    for (auto mi : mode) {
+        std::cout << mi << ", ";
+    }
+    std::cout << "\n";
+    
     return 0;
 }
+
+void _parseSection(const std::string &section, std::vector<int64_t> &result) {
+    std::stringstream ss(section);
+    std::string item;
+    
+    while (std::getline(ss, item, ',')) {
+        result.push_back(std::stoi(item));
+    }
+}
+
+void parseSignal(const std::string &s, std::vector<int64_t> &time, std::vector<int64_t> &mode) {
+    // clearing values
+    time.clear();
+    mode.clear();
+    
+    // parsing new values
+    size_t semicolonPos = s.find(';');
+    if (semicolonPos == std::string::npos) {
+        throw std::runtime_error("No semicolon found");
+    }
+    
+    // extract the parts
+    std::string timeSection = s.substr(0, semicolonPos);
+    std::string modeSection = s.substr(semicolonPos + 1);
+    
+    // parse each part
+    _parseSection(timeSection, time);
+    _parseSection(modeSection, mode);
+}
+
 
 void PrintV(int V[], int n) {
     std::cout << "V: ";
