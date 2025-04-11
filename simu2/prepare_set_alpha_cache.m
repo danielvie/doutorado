f08ffd66314ece3e0b26e0291c0ea2f2cb7bdf9a
Daniel Vieira <danielvie@gmail.com>
2025-04-11 00:40:05 -0300

    improve `prepare_set_alpha_cache`

--- simu2/prepare_set_alpha_cache.m
+++ simu2/prepare_set_alpha_cache.m
@@ -1,14 +1,24 @@
+function prepare_set_alpha_cache()
 
-cont = 0;
-freq = 10;
+    s = Simulation();
+    success = s.set_config(Enums.SimName.LAB_CIRCUIT);
 
-h = waitbar(0, 'Processing...');
-for alpha = 0.001:0.001:0.999
-    cont = rem(cont + 1, freq);
-    if cont == 0
-        waitbar(alpha/0.999, h, 'Processing...');
+    if ~success
+        error("ERROR: could not set config");
     end
+    
+    cont = 0;
+    freq = 10;
+    
+    h = waitbar(0, 'Processing...');
+    for alpha = 0.01:0.01:0.99
+        cont = rem(cont + 1, freq);
+        if cont == 0
+            waitbar(alpha/0.999, h, 'Processing...');
+        end
+    
+        s.set_alpha_and_mpc_cached(alpha);
+    end
+    close(h);
 
-    s.set_alpha_and_mpc_cached(alpha);
 end
-close(h);
