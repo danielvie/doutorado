75cbbe1a16f3b84f051b23ab866fff0cd790190d
Daniel Vieira <danielvie@gmail.com>
2025-06-22 22:43:58 -0300

    add compute_ts_from_dtk

---
+++ simu2/@Simulation/compute_ts_from_dtk.m
@@ -0,0 +1,15 @@
+function Ts = compute_ts_from_dtk(self, config, dtk)
+    % Applies time adjustments based on control signal dtk
+    % Returns the adjusted time vector
+    
+    % start with base time vector
+    Ts = config.Ts;
+    
+    % apply quantization
+    Ts = self.quantizacao(Ts, Enums.QuantType.Sim);
+    
+    % adjust time vector based on control signal
+    for j = 1:numel(dtk)
+        Ts(j+1) = Ts(j+1) + dtk(j);
+    end
+end
