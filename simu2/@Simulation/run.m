3132ba8e3e237072efea86af2975d865e085472c
Daniel Vieira <danielvie@gmail.com>
2025-08-01 08:58:10 -0300

    iter

--- simu2/@Simulation/run.m
+++ simu2/@Simulation/run.m
@@ -68,30 +68,30 @@ function [y,t,m,dtk_out] = run(self, nsim)
             % constrainting `dtk`
             time_us = self.get_time_us();
             dtk_us = dtk*1e6;
-            dtk_us(1) = max(time_us(2)-time_us(1) + dtk_us(1),5);
-            dtk_us(2) = max(time_us(3)-time_us(2) + dtk_us(1)+dtk_us(2),5);
-            dtk_us(3) = max(time_us(4)-time_us(3) + dtk_us(1)+dtk_us(2)+dtk_us(3),5);
-            dtk_us(4) = max(time_us(5)-time_us(4) + dtk_us(1)+dtk_us(2)+dtk_us(3)+dtk_us(4),5);
-            dtk_us(5) = max(time_us(6)-time_us(5) + dtk_us(1)+dtk_us(2)+dtk_us(3)+dtk_us(4)+dtk_us(5),5);
+            % dtk_us(1) = max(time_us(2)-time_us(1) + dtk_us(1),5);
+            % dtk_us(2) = max(time_us(3)-time_us(2) + dtk_us(1)+dtk_us(2),5);
+            % dtk_us(3) = max(time_us(4)-time_us(3) + dtk_us(1)+dtk_us(2)+dtk_us(3),5);
+            % dtk_us(4) = max(time_us(5)-time_us(4) + dtk_us(1)+dtk_us(2)+dtk_us(3)+dtk_us(4),5);
+            % dtk_us(5) = max(time_us(6)-time_us(5) + dtk_us(1)+dtk_us(2)+dtk_us(3)+dtk_us(4)+dtk_us(5),5);
             
             Ts = self.compute_ts_from_dtk(self.m_config, dtk_us*1e-6);
 
-            % 
-            % c = self.m_config.c_time(1);
-            % 
-            % while any(diff(Ts) < 0)
-            %     dts = max(diff(Ts), c);
-            %     dts_ = dts - diff(Ts);
-            %     for j = 1:numel(dts_)
-            %         Ts(j+1) = Ts(j+1) + dts_(j);
-            %     end
-            % 
-            %     % conditioning time to factible values
-            %     Ts_final = self.m_config.Ts(end);
-            %     if sum(Ts) > Ts_final
-            %         Ts = Ts/norm(Ts)*Ts_final;
-            %     end
-            % end
+
+            c = self.m_config.c_time(1);
+
+            while any(diff(Ts) < 0)
+                dts = max(diff(Ts), c);
+                dts_ = dts - diff(Ts);
+                for j = 1:numel(dts_)
+                    Ts(j+1) = Ts(j+1) + dts_(j);
+                end
+
+                % conditioning time to factible values
+                Ts_final = self.m_config.Ts(end);
+                if sum(Ts) > Ts_final
+                    Ts = Ts/norm(Ts)*Ts_final;
+                end
+            end
 
             ts_us = Ts*1e6;
             d_ts_us = diff(ts_us);
