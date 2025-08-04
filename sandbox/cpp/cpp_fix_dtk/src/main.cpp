7652983dfe715b44abf19f57dc97dedacd58ec14
Daniel Vieira <danielvie@gmail.com>
2025-08-03 23:46:48 -0300

    iter

--- sandbox/cpp/cpp_fix_dtk/src/main.cpp
+++ sandbox/cpp/cpp_fix_dtk/src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <numeric>
 #include <string>
+#include <chrono>
 
 // time_us =
 //     84     9    84     9    84     9
@@ -33,13 +34,6 @@ void vector_print(const std::vector<float>& V, const std::string name) {
 
 Result fix_dtk(const std::vector<float>& time_us, const float& time_constraint_us, std::vector<float>& dtk_us) {
     
-    
-    vector_print(time_us, "time_us");
-    vector_print(dtk_us, "dtk_us");
-
-    std::cout << "time_constraint_us: ";
-    std::cout << time_constraint_us << "\n";
-
     size_t dtk_len   = dtk_us.size();
     size_t ts_us_len = time_us.size()+1;
 
@@ -80,7 +74,6 @@ Result fix_dtk(const std::vector<float>& time_us, const float& time_constraint_u
         }
     }
 
-
     // impose time constraint for `ts_us`
     for (size_t i = 0; i < ts_us_len-1; i++) {
         if (ts_us[i+1] - ts_us[i] < time_constraint_us) {
@@ -88,15 +81,10 @@ Result fix_dtk(const std::vector<float>& time_us, const float& time_constraint_u
         }
     }
 
-
     for (size_t i = 0; i < dtk_len; i++) {
         dtk_us[i] = ts_us[i+1] - ts_us_ref[i+1];
     }
     
-    vector_print(dtk_us, "dtk_us: ");
-    vector_print(ts_us, "ts_us: ");
-
-
     return Result::OK;
 }
 
@@ -107,15 +95,22 @@ int main() {
     std::vector<float> dtk_us = {4146.18386384488, 4783.98329645644, -4771.28079764979, 5826.11363501824,-5782.47667405956};
     float time_constraint_us = 3;
 
+    vector_print(dtk_us, "dtk_us: ");
 
+    auto clock_ini = std::chrono::high_resolution_clock::now();
     auto err = fix_dtk(time_us, time_constraint_us, dtk_us);
+    auto clock_end = std::chrono::high_resolution_clock::now();
+
+    vector_print(dtk_us, "dtk_us_new: ");
 
     if (err == Result::FAIL) {
         std::cout << "deu ruim!\n";
         return 0;
     }
 
-    std::cout << "hello you\n";
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(clock_end - clock_ini);
+    std::cout << "duration: ";
+    std::cout << duration.count() << " us\n";
 
     return 0;
 }
