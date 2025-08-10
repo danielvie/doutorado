e633b7d776ff536b1f212f3030cdb6cf25125594
Daniel Vieira <danielvie@gmail.com>
2025-08-10 10:18:41 -0300

    moving folders

--- play/cpp_test/lib/utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdlib>
-#include <iostream>
-#include <sstream>
- 
-void GetValues(const char *s, int timeValues[], int modeValues[]);
-void Init(int timeValues[], int modeValues[], int N);
-void Print(int timeValues[], int modeValues[], int N);
