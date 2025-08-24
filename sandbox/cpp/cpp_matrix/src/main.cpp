f8938fa445dd9b487ef44807303665f5d1a30170
Daniel Vieira <danielvie@gmail.com>
2025-08-23 21:51:44 -0300

    iter

--- sandbox/cpp/cpp_matrix/src/main.cpp
+++ sandbox/cpp/cpp_matrix/src/main.cpp
@@ -3,39 +3,6 @@
 
 int main() {
 
-    // MatrixData M;
-    // M.rows = 5;
-    // M.cols = 3;
-
-    // M.values.push_back(-0.0023027);
-    // M.values.push_back(-0.0045131);
-    // M.values.push_back(-0.0045605);
-    // M.values.push_back(0.0023013);
-    // M.values.push_back(0.0045863);
-    // M.values.push_back(0.0023314);
-    // M.values.push_back(-0.0022593);
-    // M.values.push_back(-0.0090792);
-    // M.values.push_back(-0.0022091);
-    // M.values.push_back(0.0022807);
-    // M.values.push_back(-0.028284);
-    // M.values.push_back(0.028502);
-    // M.values.push_back(-0.056414);
-    // M.values.push_back(0.029898);
-    // M.values.push_back(-0.029624);
-    
-    // matrix_print(M);
-
-
-    // float result[5];
-    // matrix_multiply_vector3(M, 1, 1, 1, result);
-
-    // std::cout << "result: ";
-    // for (size_t i = 0; i < 5; i++)
-    // {
-    //     printf("%f ", result[i]);
-    // }
-    // printf(";");
-    
     matrix_test();
 
     return 0;
