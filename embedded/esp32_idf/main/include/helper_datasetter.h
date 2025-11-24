e0e5c31508d76888ed15ba752dd84f21e20ba078
Daniel Vieira <danielvie@gmail.com>
2025-11-23 22:56:42 -0300

    iter

---
+++ embedded/esp32_idf/main/include/helper_datasetter.h
@@ -0,0 +1,8 @@
+// Copyright 2025 ITA (Instituto Tecnologico de Aeronautica). Licensed under the MIT license.
+
+#pragma once
+
+#include "signal_controller.h"
+
+void helper_set_dataset_from_alpha(DataSet *dataset, float alpha);
+bool helper_eql_float(const float a, const float b);
