c8c302ee4e7cb4a67925acc212095891eef9dd6f
Daniel Vieira <danielvie@gmail.com>
2026-05-21 07:25:48 -0300

    pre reuniao

--- z_kawa/signal_generate.m
+++ z_kawa/signal_generate.m
@@ -1,4 +1,4 @@
-function [u1, u2, u3, u] = signal_generate(time_us, mode, t_s)
+function [u3High, u3Low ,u2High, u2Low ,u1High, u1Low, u] = signal_generate(time_us, mode, t_s)
 
     t_us = t_s*1e6;
 
@@ -19,5 +19,15 @@ function [u1, u2, u3, u] = signal_generate(time_us, mode, t_s)
 
     % Reconstruct the active mode from the output bits.
     u = u1 + 2*u2 + 4*u3;
+
+    % Creating High/Low signals
+    u3High = u3*2 - 1;
+    u3Low  = (1-u3)*2 - 1;
+    u2High = u2*2 - 1;
+    u2Low  = (1-u2)*2 - 1;
+    u1High = u1*2 - 1;
+    u1Low  = (1-u1)*2 - 1;
+
+
 
 end
